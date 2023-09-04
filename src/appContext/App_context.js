3c1759b4584d7bc8258bb7ff373cece54da48ce6
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-09-04 17:02:25 -0400

    working on create post route

--- src/appContext/App_context.js
+++ src/appContext/App_context.js
@@ -10,50 +10,15 @@ const AppContextProvider = (props)=>{
     const [musiq,setMusiq]=useState(null)
     const [post,setPost]=useState([])
     const [postUser,setPostUser]=useState([])
-    // const getMusiq =async (searchTerm)=>{
-    //     const response = await axios.get(`https://shazam.p.rapidapi.com/search?term=${search}`,
-    //     // {params: {term: ''}},
-    //     {headers: {
-    //         'X-RapidAPI-Key': 'e92eec9951mshaba45c3344a2ad6p1f233ejsna6033bf06696',
-    //         'X-RapidAPI-Host': 'shazam.p.rapidapi.com'}
-    //       })
-    //     let res = response.data
-    //     console.log(res)
-    //     setMusiq(res)
-    // }
-    // const getPost= async()=>{
-    //   const res =await axios.get("api/post/feed")
-    //   console.log(res)
-    //  setPost(res.data)
-    // }
-    // musiqSearch = () => {
-
-    // }
-  
+    
     useEffect(()=> {
         async function getPost() {
           const post = await postAPI.getAll();
-        
-         
           setPost(post);
-         
         }
         getPost();
         }, []);
-    // useEffect(()=> {
-    //     async function getPostUser() {
-    //       const post = await postAPI.getById();
-        
-         
-    //       setPostUser(post);
-         
-    //     }
-    //     getPost();
-    //     }, [post.userID]);
-    // useEffect(()=>{
-    //   getMusiq()
-    //   // getPost()
-    //  },[])
+    
     return(
         <AppContext.Provider value={{post,setPost,postUser,setPostUser}}>
          {props.children}
@@ -62,34 +27,54 @@ const AppContextProvider = (props)=>{
 }
 export default AppContextProvider
 // const options = {
-//     method: 'GET',
-//     url: 'https://shazam.p.rapidapi.com/charts/track',
-//     headers: {
-//       'X-RapidAPI-Key': 'e92eec9951mshaba45c3344a2ad6p1f233ejsna6033bf06696',
-//       'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-//     }
-//   };
-  
-//   axios.request(options).then(function (response) {
-//     console.log(response.data);
-//   }).catch(function (error) {
-//     console.error(error);
+    //     method: 'GET',
+    //     url: 'https://shazam.p.rapidapi.com/charts/track',
+    //     headers: {
+        //       'X-RapidAPI-Key': 'e92eec9951mshaba45c3344a2ad6p1f233ejsna6033bf06696',
+        //       'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+        //     }
+        //   };
+        
+        //   axios.request(options).then(function (response) {
+            //     console.log(response.data);
+            //   }).catch(function (error) {
+                //     console.error(error);
 //   });
 // search
 // import axios from "axios";
 
 // const options = {
-//   method: 'GET',
-//   url: 'https://shazam.p.rapidapi.com/search',
-//   params: {term: 'kendrick lamar'},
-//   headers: {
-//     'X-RapidAPI-Key': 'e92eec9951mshaba45c3344a2ad6p1f233ejsna6033bf06696',
-//     'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-//   console.log(response.data);
-// }).catch(function (error) {
-//   console.error(error);
-// });
+    //   method: 'GET',
+    //   url: 'https://shazam.p.rapidapi.com/search',
+    //   params: {term: 'kendrick lamar'},
+    //   headers: {
+        //     'X-RapidAPI-Key': 'e92eec9951mshaba45c3344a2ad6p1f233ejsna6033bf06696',
+        //     'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+        //   }
+        // };
+        
+        // axios.request(options).then(function (response) {
+            //   console.log(response.data);
+            // }).catch(function (error) {
+                //   console.error(error);
+                // });
+                // -------------------------------------------shazmam music api
+                // const getMusiq =async (searchTerm)=>{
+                //     const response = await axios.get(`https://shazam.p.rapidapi.com/search?term=${search}`,
+                //     // {params: {term: ''}},
+                //     {headers: {
+                //         'X-RapidAPI-Key': 'e92eec9951mshaba45c3344a2ad6p1f233ejsna6033bf06696',
+                //         'X-RapidAPI-Host': 'shazam.p.rapidapi.com'}
+                //       })
+                //     let res = response.data
+                //     console.log(res)
+                //     setMusiq(res)
+                // }
+                // const getPost= async()=>{
+                //   const res =await axios.get("api/post/feed")
+                //   console.log(res)
+                //  setPost(res.data)
+                // }
+                // musiqSearch = () => {
+                
+                // }
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,26 +1,36 @@
-import {useContext} from 'react'
+import {useContext,useEffect} from 'react'
 import { AppContext } from '../../appContext/App_context'
+import axios from 'axios'
 
-function Post() {
-    let {musiq} = useContext(AppContext)
-    let {search,setSearch} = useContext(AppContext)
-    let {getMusiq} = useContext(AppContext)
-    // let {setPost} = useContext(AppContext)
+function Post({user}) {
+    // let {musiq} = useContext(AppContext)
+    // let {search,setSearch} = useContext(AppContext)
+    // let {getMusiq} = useContext(AppContext)
+    let {setPost,post} = useContext(AppContext)
     const handleSubmit = (e)=>{
     e.preventDefault()
    
 }
   const handleChange = (e)=>{
-    setSearch(e.target.value)
+    // setSearch(e.target.value)
     // getMusiq(search)
     }
+    useEffect(()=> {
+      async function getPost() {
+        axios.get('http://localhost:3000/api//posts/new')
+        .then(post => setPost(post.data))
+        .catch(err => console.log(err))
+      }
+      
+      }, []);
   return (
     <div className='post'>
    <a href='/'>X</a>
    <h1>Create Post</h1>
    <form onSubmit={handleSubmit} className='form' action='/post' method='POST'>
+    userID:{user._id.name}
     image:<input type='text' name='image'/>
-    song:<input type='text' name='song'onChange={handleChange} value={search}/>
+    song:<input type='text' name='song'onChange={handleChange} />
     <input className='subBtn' type="submit" name="" value=" SUBMIT"/>
    </form>
    {/* {musiq?( musiq.map((tracks,i)=>{
