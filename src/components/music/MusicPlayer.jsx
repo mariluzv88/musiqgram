63b2cc8240bf285e7103ed444195ae8a9742a513
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-09-09 21:53:29 -0400

    player base started

--- src/components/music/MusicPlayer.jsx
+++ src/components/music/MusicPlayer.jsx
@@ -1,13 +1,14 @@
 import {useContext,useEffect,useState} from 'react'
 import { AppContext } from '../../appContext/App_context'
+import {  FaForward,FaStepForward, FaStepBackward,FaBackward,FaPlay,FaPause,} from 'react-icons/fa'
 
 function MusicPlayer() {
     let {title,setTitle} = useContext(AppContext)
     let {image,setImage} = useContext(AppContext)
     let {song,setSong} = useContext(AppContext)
   return (
-    <div>
-
+    <div className='player'>
+     <audio src={song} preload='metadata' />
     </div>
   )
 }
